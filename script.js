68e1d23ac8dc64f8462bb0ca74264554c048f452
Abbos <abbos.pankiy2018@gmail.com>
2024-03-16 19:26:21 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// let users = [
+//     {id: 1, name:'Kamol'},
+//     {id: 1, name:'Abbos'},
+//     {id: 1, name:'Someone'},
+// ]
+// const onSearch = (input) => {
+//     let result = [];
+//     for (let i = 0; i < users.length; i++){
+//     if(users[i].name.toLocaleLowerCase().includes(input.toLocaleLowerCase())){
+//         result.push(users[i])
+//     }
+//   }
+//   console.log(result);
+// }
+// onSearch('s')
+
+// let str = 'ABDBACC';
+
+// let arr = [];
+
+// for(let i = 0; i <str.length; i++){
+//     // console.log(arr, str[i]);
+
+//     if(arr.includes(str[i])){
+//     console.log((str[i])); break
+// }
+//     arr.push(str[i])
+// }
